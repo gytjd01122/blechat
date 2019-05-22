7445e3441efc2bbd3b6b19cab5f2eeb47fbbd63f
KIM HYO SEONG <5171699@stu.kmu.ac.kr>
2019-05-22 19:24:50 +0900

    수정함: (Appdatabase): db이름 변경, (Gps): id 필드 추가.

--- app/src/main/java/com/hardcopy/blechat/db/AppDatabase.java
+++ app/src/main/java/com/hardcopy/blechat/db/AppDatabase.java
@@ -38,7 +38,7 @@ public abstract class AppDatabase extends RoomDatabase {
 
         if (INSTANCE == null) {
             INSTANCE =
-                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "user-database")
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "db")
                             // allow queries on the main thread.
                             // Don't do this on a real app! See PersistenceBasicSample for an example.
                             .allowMainThreadQueries()
