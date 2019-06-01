85cae295bb65f7d9954a7518e45fbdea90558ffb
KIM HYO SEONG <5171699@stu.kmu.ac.kr>
2019-06-02 04:45:54 +0900

    수정함: 운동 액티비티 구현

--- app/src/main/java/com/hardcopy/blechat/ExerciseActivity.java
+++ app/src/main/java/com/hardcopy/blechat/ExerciseActivity.java
@@ -3,29 +3,40 @@ package com.hardcopy.blechat;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
+import com.hardcopy.blechat.db.AppDatabase;
+import com.hardcopy.blechat.exerciseLogic.ExerciseLogic;
 
 import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 
-
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class ExerciseActivity extends AppCompatActivity {
 
+
         /*주석뒤에 (..)표시된거는 DB가 있으면 없어도 되는 코드일거에요!*/
+
+        ExerciseLogic logic;
 
         ArrayList<Event> eventlist = new ArrayList<>(); //점찍는 이벤트가 들어갈 list
         ArrayList<String> ExerciseDate = new ArrayList<>();  //DB에서 받아온 "yyyy-MM-dd"형식의 string에 " 09:00:00"을 더해서 "yyyy-MM-dd HH:mm:ss"형식으로 바뀐 값을 넣는 list
         ArrayList<Long> millisecond = new ArrayList<>();    //"yyyy-MM-dd HH;mm:ss"형식을 mills로 바꾼 값을 넣는 list
+
+        List<String> exerciseDays;
 
         CompactCalendarView compactCalendar;
         SimpleDateFormat dateFormatMonth = new SimpleDateFormat("yyyy-MM", Locale.getDefault());
@@ -33,19 +44,35 @@ public class ExerciseActivity extends AppCompatActivity {
         TextView myDate;
         TextView Month;
 
+        TextView distanceView;
+        TextView timeView;
+        TextView kcalView;
+/*
         int a = 1;//if문 실행위해 임의로 만든 변수(..)
         String Edate = "2019-05-24";  //"Edate= DB에서 받아온 날짜 형식"
-
+*/
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_exercise);
 
+            Date today = new Date();
+            logic = new ExerciseLogic(this, MainActivity.db, simpleDateFormat.format(today));
+
             Month = (TextView) findViewById(R.id.yyyy_mm);
             myDate = (TextView) findViewById(R.id.myDate);
+
+            distanceView = (TextView) findViewById(R.id.Distance);
+            timeView = (TextView) findViewById(R.id.Time);
+            kcalView = (TextView) findViewById(R.id.Kcal);
+
             compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
             compactCalendar.setFirstDayOfWeek(Calendar.SUNDAY);
             compactCalendar.setUseThreeLetterAbbreviation(true);
+
+
+
+
 
         /*
         DB에 입력된 거리 값이 0이 아닌 것들을 다 읽어오는 쿼리 필요
@@ -55,7 +82,25 @@ public class ExerciseActivity extends AppCompatActivity {
         calendar에 이벤트 붙이기
         */
 
+        //DB에서 받아온 "yyyy-MM-dd"형식의 string에 " 09:00:00"을 더해서 "yyyy-MM-dd HH:mm:ss"형식으로 바꾸어서 list에 넣음.
+        exerciseDays = MainActivity.db.gpsDao().getAllExerciseDay();
+        Iterator<String> it =  exerciseDays.iterator();
+        while (it.hasNext()) {
+            String _str = it.next();
+            ExerciseDate.add( _str +" 09:00:00");
+        }
 
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");     //"yyyy-MM-dd HH:mm:ss"형식의 string값을 date형으로 변환
+            for (String s : ExerciseDate) {
+                Date date = sdf.parse(s);
+                millisecond.add(date.getTime());    //date형을 mills로 변환해서 list에 넣기
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+/*
             //DB에서 받아온 "yyyy-MM-dd"형식의 string에 " 09:00:00"을 더해서 "yyyy-MM-dd HH:mm:ss"형식으로 바꾸어서 list에 넣음.
             while (ExerciseDate.size() >= 0) {//while(DB에 입력된 거리값이 0이 아닌 것들을 다 읽어올때까지)
 
@@ -65,6 +110,7 @@ public class ExerciseActivity extends AppCompatActivity {
                     a--;
                 } else if (a == 0) break;
             }
+
 
             try {
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");     //"yyyy-MM-dd HH:mm:ss"형식의 string값을 date형으로 변환
@@ -76,7 +122,7 @@ public class ExerciseActivity extends AppCompatActivity {
                 e.printStackTrace();
                 ;
             }
-
+*/
             //점찍는 이벤트 생성하고 list에 insert
             for (Long l : millisecond) {
                 eventlist.add(new Event(Color.GREEN, l, "exercise day"));
@@ -92,6 +138,20 @@ public class ExerciseActivity extends AppCompatActivity {
                 public void onDayClick(Date dateClicked) {
                     Context context = getApplicationContext();
                     myDate.setText(simpleDateFormat.format(dateClicked));
+
+                    String _date = new SimpleDateFormat("yyyy-MM-dd").format(dateClicked);
+
+                    if (!exerciseDays.contains(_date)) {
+                        distanceView.setText("0");
+                        timeView.setText("0");
+                        kcalView.setText("0");
+
+                        return;
+                    }
+
+                    distanceView.setText( String.format( "%.2f" , MainActivity.db.gpsDao().getSumDistanceByDate(_date)) );
+                    timeView.setText(MainActivity.db.gpsDao().getSumTimeByDate(_date).toString());
+                    kcalView.setText(logic.getKcal(_date).toString());
                 }
 
                 @Override
