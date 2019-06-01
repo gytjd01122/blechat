219f2e58bef512780a5374738521223e5e14b2c9
KIM HYO SEONG <5171699@stu.kmu.ac.kr>
2019-06-01 20:40:54 +0900

    수정함: UserSetting 클래스 수정

--- app/src/main/java/com/hardcopy/blechat/setting/UserSetting.java
+++ app/src/main/java/com/hardcopy/blechat/setting/UserSetting.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 public class UserSetting {
 
     private static final String USER_SETTING = "userSetting";
+    public static final String USER_SETTING_DEFAULT_VALUE = "0";
 
     private enum USER_SETTING_KEY {
         NAME , AGE , WEIGHT , HEIGHT
@@ -23,7 +24,7 @@ public class UserSetting {
     }
 
     public static String getName() {
-        return pref.getString(USER_SETTING_KEY.NAME.toString(),"");
+        return pref.getString(USER_SETTING_KEY.NAME.toString(),USER_SETTING_DEFAULT_VALUE);
     }
 
     public void setName(String name) {
@@ -32,29 +33,29 @@ public class UserSetting {
     }
 
     public static Integer getAge() {
-        return pref.getInt(USER_SETTING_KEY.AGE.toString(),0);
+        return pref.getInt(USER_SETTING_KEY.AGE.toString(),Integer.valueOf(USER_SETTING_DEFAULT_VALUE));
     }
 
-    public void setAge(Integer age) {
-        editor.putInt(USER_SETTING_KEY.NAME.toString(), age);
+    public void setAge(String age) {
+        editor.putInt(USER_SETTING_KEY.AGE.toString(), Integer.valueOf(age));
         editor.commit();
     }
 
     public static Float getWeight() {
-        return pref.getFloat(USER_SETTING_KEY.WEIGHT.toString(),0);
+        return pref.getFloat(USER_SETTING_KEY.WEIGHT.toString(), Float.valueOf(USER_SETTING_DEFAULT_VALUE));
     }
 
-    public void setWeight(Float weight) {
-        editor.putFloat(USER_SETTING_KEY.WEIGHT.toString(), weight);
+    public void setWeight(String weight) {
+        editor.putFloat(USER_SETTING_KEY.WEIGHT.toString(), Float.valueOf(weight));
         editor.commit();
     }
 
     public static Float getHeight() {
-        return pref.getFloat(USER_SETTING_KEY.HEIGHT.toString(),0);
+        return pref.getFloat(USER_SETTING_KEY.HEIGHT.toString(),  Float.valueOf(USER_SETTING_DEFAULT_VALUE));
     }
 
-    public void setHeight(Float height) {
-        editor.putFloat(USER_SETTING_KEY.HEIGHT.toString(), height);
+    public void setHeight(String height) {
+        editor.putFloat(USER_SETTING_KEY.HEIGHT.toString(), Float.valueOf(height));
         editor.commit();
     }
 }
